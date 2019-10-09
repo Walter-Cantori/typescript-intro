51b6099e2478e554621113c876d65f2a13ea0816
walter <walter.cantori@gmail.com>
2019-10-08 22:35:08 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// function foo(n1: number, n2: number): number {
+//   return n1 + n2;
+// }
+
+// console.log(foo(1, 2));
+
+console.log("b" + "a" + +"a" + "a");
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    publicPath: "/",
+    contentBase: "./dist",
+    hot: true
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  }
+};
